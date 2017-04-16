c8d1f8d89c0abdeab50c3fdd72ae30bdb72bfec4
test <422538722@qq.com>
2017-04-16 16:57:05 +0800

    17.5.3
    CMDeviceMotion

---
+++ 17.5.3 CMDeviceMotion/17.5.3 CMDeviceMotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17.5.3 CMDeviceMotion
+//
+//  Created by 李维佳 on 2017/4/14.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
